685a9424e40f5a2c6e1f03c908abf3a7a5280487
Duyet Nguyen React+ <88522317+duyetrubyonring@users.noreply.github.com>
2021-10-13 10:06:51 +0700

    apply coreui theme (#1)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link @coreui/icons/sprites/free.svg
+//= link @coreui/icons/sprites/brand.svg
+//= link @coreui/icons/sprites/flag.svg
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
+Rails.application.config.assets.precompile += ['brand/coreui.svg']
+
+# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+lib_files = Dir[Rails.root.join('node_modules', '@coreui', 'icons', 'svg', '**', '[^_]*.svg')]
+lib_files.map! { |file| file.sub(%r(#{Rails.root.join('public')}), '') }
+Rails.application.config.assets.precompile += lib_files
